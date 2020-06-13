58339653ebdef5895ed0cd6bb14860fcda687a81
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-13 23:18:46 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word(string)
+#Turn string into an array
+string.split(",")
+
+#Iterate each element in the array to reverse letter order
+string.each do |word|
+  puts word.reverse 
+  end
+#Turn array back to a string
+end
